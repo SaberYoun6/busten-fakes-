b6f0e9f1b375a1d3280d488b4e70d7257ec4aaa4
yuriprym <samuel.young.103@gmail.com>
2018-04-07 15:52:01 -0400

    can read into btye format form image

--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 import "fmt"
-
+import "io/ioutil"
+// this function is here to see if any error will show up
+func Check(e error) error
+{
+	// this is used to see if nothing isn't found and then return a nil if there are things found
+	if err != nil {
+		return e
+	}
+}
 
 
 func main(){
+	// this create two variable one of which is bitslice and they other ie err 
+	bitslice, err :=  ioutil.ReadFile("/home/winnethepooh/Pictures/alter.jpg")
+	Check(err)
+	// this does the samething as a above
+	byteslice, err:= ioutil.ReadFile("/home/winnethepooh/Pictures/2017-17-24-0645445.jpg")
+	// this proved that if there is no error then the result will work
+	Check(err)
+	s  := make([]byte,len(bitslice))
+	sc := make([]byte,len(byteslice))
+	for i :=0 ; i <= len(bitslice){
+		s[i] 
+		for j :=  0; j <=len(byteslice){	
+			if  {
 
+			}
+			else if {
+
+			}
+			else {
+			}
+
+
+		}
+
+	}
+	fmt.Print(bitslice)
 }
+
+
+
+
